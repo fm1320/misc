04a79c6a52c71da108ccddd97de35850316f16dc
fm1320 <73337655+fm1320@users.noreply.github.com>
2021-03-27 02:04:17 +0100

    Add files via upload

--- algo.py
+++ algo.py
@@ -2,10 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import altair as alt
-import pandas_profiling
-import streamlit_pandas_profiling
-from pandas_profiling import ProfileReport
-from streamlit_pandas_profiling import st_profile_report
+# from pandas_profiling import ProfileReport
+# from streamlit_pandas_profiling import st_profile_report
 
 st.title('The ultimate visualization experience')
 
@@ -17,8 +15,20 @@ def load_data(nrows):
     data = df = pd.read_csv(DATA_URL,nrows=nrows) 
     return data
 
+
+# past `x` days slider and display
+st.sidebar.write("""
+## Select the number of rows
+""")
+
 data_load_state = st.text('Loading data...')
-data = load_data(500)
+rows = st.sidebar.slider("No. of rows ", 50, 100, 500, 1000, 2000)
+data = load_data(rows)
+
+st.write(f"""
+### Loaded **{rows}** rows
+""")
+
 data_load_state.text("Done! (using st.cache)")
 
 if st.checkbox('Show raw data'):
@@ -33,9 +43,9 @@ st.subheader('Some linecharts')
 st.line_chart(data)
 st.area_chart(data)
 
-pr = ProfileReport(data, explorative=True)
-st.header('**Pandas Profiling Report**')
-st_profile_report(pr)
+# pr = ProfileReport(data, explorative=True)
+# st.header('**Pandas Profiling Report**')
+# st_profile_report(pr)
 
 st.write("Other possibilites with data")
 df1 = pd.DataFrame(np.random.randn(200, 3),columns=['a', 'b', 'c'])
