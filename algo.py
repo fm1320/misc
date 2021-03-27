1fd9c564f43c58364ec4e7371a12bb076b8f2443
fm1320 <73337655+fm1320@users.noreply.github.com>
2021-03-27 01:55:30 +0100

    Add files via upload

--- algo.py
+++ algo.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import altair as alt
+import pandas_profiling
+import streamlit_pandas_profiling
 from pandas_profiling import ProfileReport
 from streamlit_pandas_profiling import st_profile_report
 
