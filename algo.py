832a4b0e1d1c8986c1db878f5b6fa884d4aaff98
fm1320 <73337655+fm1320@users.noreply.github.com>
2021-03-27 02:18:10 +0100

    Add files via upload

--- algo.py
+++ algo.py
@@ -2,9 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import altair as alt
-# from pandas_profiling import ProfileReport
-# from streamlit_pandas_profiling import st_profile_report
-
 st.title('The ultimate visualization experience')
 
 DATE_COLUMN = 'Series1'
@@ -16,31 +13,23 @@ def load_data(nrows):
     return data
 
 data_load_state = st.text('Loading data...')
-data = load_data(500)
+data = load_data(100)
 data_load_state.text("Done! (using st.cache)")
 
 if st.checkbox('Show raw data'):
     st.subheader('Raw data')
     st.write(data)
 
-bins_slide = st.sidebar.slider("Graph width ", 5, 10, 20)
-
-
 st.subheader('Some barcharts')
-hist_values = np.histogram(data[DATE_COLUMN], bins=bins_slide)[0]
+hist_values = np.histogram(data[DATE_COLUMN], bins=10)[0]
 st.bar_chart(hist_values)
 
 st.subheader('Some linecharts')
-st.line_chart(data)
-st.area_chart(data)
+st.line_chart(data, columns=['Series1'])
+st.area_chart(data, columns=['Series1'])
 
-# pr = ProfileReport(data, explorative=True)
-# st.header('**Pandas Profiling Report**')
-# st_profile_report(pr)
-
-st.write("Other possibilites with data")
-df1 = pd.DataFrame(np.random.randn(200, 3),columns=['a', 'b', 'c'])
-c = alt.Chart(df1).mark_circle().encode(x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+df = pd.DataFrame(data,columns=['Series1', 'b', 'c'])
+c = alt.Chart(df).mark_circle().encode(x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
 st.altair_chart(c, use_container_width=True)
 
 #Some number in the range 0-23
